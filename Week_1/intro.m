471e00aded0538c733711a8a844abd1c298dfaba
Acefinfo <fromaashutoshthapa@gmail.com>
2026-02-15 09:03:35 +0545

    Simple introduction to matlab

---
+++ Week_1/intro.m
@@ -0,0 +1,17 @@
+a = 5;          % Assign value 5 to variable 'a'
+b = 10;         % Assign value 10 to variable 'b'
+c = a + b;      % Add 'a' and 'b'
+disp(c);        % Display the value of 'c'
+
+% Task 1:  Now write a set of commands for equation: Y=(A*B)+(C-D)/X with appropriate values for variables
+
+a = 10;
+b = 20;
+c = 30;
+d = 5;
+x = 2;
+
+Y = (a*b) + (c-d)/2;
+disp(Y);
+
+
