55b29ac3cfe631bcc91b0858c427ffcf7e3ee993
Acefinfo <fromaashutoshthapa@gmail.com>
2026-02-15 09:05:22 +0545

    Week 1: Complete Basics introduction to Matrics operation

---
+++ Week_1/Matrics_operation.m
@@ -0,0 +1,35 @@
+A = [1 2 3; 4 5 6; 7 8 9];
+B = [9 8 7; 6 5 4; 3 2 1];
+
+C = A + B;
+D =A-B;
+E = A*B;
+
+disp("Result of matrix addition (A + B):");
+disp(C);
+
+disp("Result of matrix subtraction (A - B):");
+disp(D);
+
+disp("Result of matrix multiplication (A * B):");
+disp(E);
+
+% Matrics Operation
+
+det_A = det(A); % Calculate the determinant of matrix A
+inv_A = inv(A); % Calculate the inverse of matrix A
+trans_A = A';   % Calculate the transpose of matrix A
+eig_A = eig(A); % Calculate the eigenvalues of matrix A
+
+disp("Determinant of matrix A:");
+disp(det_A);
+
+disp("Inverse of matrix A:");
+disp(inv_A);
+
+disp("Transpose of matrix A:");
+disp(trans_A);
+
+disp("Eigenvalues of matrix A:");
+disp(eig_A);
+
