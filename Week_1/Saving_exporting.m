434b15d46eb90541954815bca97f4a24c26a6014
Acefinfo <fromaashutoshthapa@gmail.com>
2026-02-16 08:53:47 +0545

    Exercise: saving and exporting image data

---
+++ Week_1/Saving_exporting.m
@@ -0,0 +1,21 @@
+% Saving and exporting image data in MATLAB
+
+image = imread('Asset/Asset1.jpg');  
+
+gray_img = im2double(image);
+
+R = gray_img;            % Red channel
+G = 0.5 * gray_img;      % Reduce green
+B = 1 - gray_img;        % Blue inverse
+
+color_img = cat(3, R, G, B);
+
+figure;
+imshow(color_img);
+title('Custom Colored Image');
+
+% Saging image to a file
+imwrite(color_img, 'Output/CustomColored.png')
+
+% Saving image data to a .mat file
+save('Output/image_data.mat', 'color_img');
