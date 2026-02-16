8c115b3b166e27e5ab1763da6d7a204588bac931
Acefinfo <fromaashutoshthapa@gmail.com>
2026-02-16 07:51:38 +0545

    Simple image processing

---
+++ Week_1/Basics_image.m
@@ -0,0 +1,33 @@
+
+% Basics of image processing in MATLAB
+
+% Read and display image
+image = imread('Asset/Asset1.jpg');
+imshow(image);
+
+% Adding title and labels to the image
+title('Sample image');  % Add a title to the image
+xlabel('X-axis');       % Label for X-axis
+ylabel('Y-axis');       % Label for Y-axis
+
+% Get the size of the image
+img_size = size(image);
+disp("Image dimension are:");
+disp(img_size);
+
+% Get the class of the image
+img_class = class(image);   % Get the class of the image (e.g., uint8, double, etc.)
+disp("Image class is:");    
+disp(img_class);
+
+if size(image, 3) == 1
+    disp("The image is grayscale.");
+elseif size(image, 3) == 3
+    disp("The image is RGB.");
+else
+    disp("The image has an unsupported number of channels.");
+end
+
+info = imfinfo('Asset/Asset1.jpg');  % Get information about the image file
+disp('Image metadata:');
+disp(info);
